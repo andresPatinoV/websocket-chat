cf9c3bb31cad8d5cd1e32d7a69087de8c63dab63
David Patino <deibyandresp@hotmail.com>
2023-11-08 20:26:09 -0500

    initial commit.

---
+++ server.py
@@ -0,0 +1,25 @@
+import asyncio
+import websockets
+
+clientes = set()
+
+class Cliente:
+    def __init__(self, name, websocket):
+        self.name = name
+        self.websocket = websocket
+
+async def chat_server(websocket, path):
+    clientes.add(Cliente('David', websocket))
+    try:
+        async for message in websocket:
+            print(clientes)
+            for cliente in clientes:
+                message_nuevo = f"{cliente.name}: {message}"
+                await cliente.websocket.send(message_nuevo)
+    finally:
+        clientes.remove(websocket)
+
+if __name__ == "__main__":
+    start_server = websockets.serve(chat_server, "localhost", 8300)
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
